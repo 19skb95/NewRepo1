a4ba213c312cae3a3c8446a9d13f3df96691a392
19skb95 <sushil.beura@gridinfocom.com>
2019-06-11 10:46:28 +0530

    Commiting All

---
+++ PayrollProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using FileReadWriteLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string TextToBeWritten=FileOperation.WriteFile(@"E:\testfile.txt");
+        }
+    }
+}
